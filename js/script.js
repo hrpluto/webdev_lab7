bca97564888c2e1f2ad023a179be1b7c3113fe62
Linus Jordan <linus.jordan@gmail.com>
2025-11-12 23:12:17 -0600

    finished

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 function init(){
 //add your javascrip between these two lines of code
- 
 
+var button = document.getElementById("entrybutton");
+var textInput = document.getElementById("entryinput");
 
+function myFunction() {
+  alert("Linus Jordan: " + textInput.value);
+  document.getElementById("textoutput").innerHTML = textInput.value;
+}
 
+button.addEventListener("click", myFunction);
 
-
-
-
+}
 window.addEventListener('load', init);
